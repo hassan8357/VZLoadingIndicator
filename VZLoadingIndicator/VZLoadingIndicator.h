d9e67c24e0ce65df6328ced4e19ad4d92c562340
Hassan Refaat Hassan <hassan.refaat@drbridge.com>
2017-08-01 15:09:33 +0200

    init 0.2.0

--- VZLoadingIndicator/VZLoadingIndicator.h
+++ VZLoadingIndicator/VZLoadingIndicator.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char VZLoadingIndicatorVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <VZLoadingIndicator/PublicHeader.h>
 
-
+#import "VZLoadingIndicator/UIWindow+Additions.h"
